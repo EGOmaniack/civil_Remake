6bb7fcc6b05c891c8f14f87e99d6b849428bfc41
EGOmaniack <EGOmaniack@mail.ru>
2015-04-13 22:14:29 +1000

    По клику выбирается здание

---
+++ Assets/Scripts/UI/Home_UI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Home_UI : MonoBehaviour {
+
+	public GameObject cerkle;
+	// Use this for initialization
+	void Awake() 
+	{
+		Animator anim = cerkle.gameObject.GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		//gameObject.transform.localScale = new Vector3(0.95f * x , 0.9f * y, 1); //4.7,4.5,1
+		cerkle.gameObject.GetComponent<Animator> ().SetTrigger ("GO");
+	}
+
+}
